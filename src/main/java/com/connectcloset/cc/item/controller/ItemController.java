a84c0f7d46121fe95c7b48700aa32eca12a48000
h2jinee <wjsgmlwls97@gmail.com>
2020-02-10 14:38:41 +0900

    DD

--- src/main/java/com/connectcloset/cc/item/controller/ItemController.java
+++ src/main/java/com/connectcloset/cc/item/controller/ItemController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,7 +28,7 @@ public class ItemController {
 	
 	
 	//새로 나온 상품
-	@GetMapping("/newItem.do")
+	@PostMapping("/newItem.do")
 	@ResponseBody
 	public List<Item> newItem(Item item) {
 		List<Item> list = itemService.newItemList(item);
